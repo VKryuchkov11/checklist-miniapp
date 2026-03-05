0deefd898095956558e80c065af2b51c8a59d359
VKryuchkov11 <kruckov812@gmail.com>
2026-03-05 20:57:31 +0300

    connect api

--- webapp/app.js
+++ webapp/app.js
@@ -8,7 +8,7 @@ async function authUser() {
 
     if (!user) return;
 
-    await fetch("https://checklist-api.onrender.com/auth", {
+    await fetch("https://checklist-miniapp.onrender.com/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
